7d042fcaef25cf71e2add46dae861c07d38b8233
Gurpreet Singh <gurpreet.singh1@agreeya.net>
2019-10-06 00:16:27 +0530

    Updates

---
+++ precache-manifest.c628a378adff84c29d8c81237fa14d05.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfc2416875b26f4ca708ef3aa5f8deb5",
+    "url": "/17nov2019/index.html"
+  },
+  {
+    "revision": "55a577cd4aa421ecbfdf",
+    "url": "/17nov2019/static/js/2.def50c6c.chunk.js"
+  },
+  {
+    "revision": "1e2ab107378e87490452",
+    "url": "/17nov2019/static/js/main.51f59981.chunk.js"
+  },
+  {
+    "revision": "0943eb900d8ae392a8ec",
+    "url": "/17nov2019/static/js/runtime-main.1b31b686.js"
+  }
+]);
